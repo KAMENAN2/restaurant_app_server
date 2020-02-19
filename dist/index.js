eba102f36533e1164a688a03ac975fa7383abf6d
daniel soro <tenandanielsoro@gmail.com>
2020-02-19 15:30:06 +0000

    modification index

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
+// index.js 
+// declaration des variables 
+// connexion a mongodb
+
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
