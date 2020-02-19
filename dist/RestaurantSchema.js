9dd7685ca233d5107a1e3a96a536264b4254c407
daniel soro <tenandanielsoro@gmail.com>
2020-02-19 17:10:16 +0000

    modification RestaurantSchema.js
    modification des variables

--- dist/RestaurantSchema.js
+++ dist/RestaurantSchema.js
@@ -2,9 +2,13 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+
 Object.defineProperty(exports, "__esModule", { value: true });
+
 var mongoose_1 = __importDefault(require("mongoose"));
+
 var mongoose_paginate_1 = __importDefault(require("mongoose-paginate"));
+
 var restaurantSchema = new mongoose_1.default.Schema({
     address: { building: String, coord: [Number, Number], street: String, zipcode: String },
     borough: String,
@@ -14,6 +18,8 @@ var restaurantSchema = new mongoose_1.default.Schema({
     restaurant_id: String
 });
 restaurantSchema.plugin(mongoose_paginate_1.default);
+
 var Restaurants = mongoose_1.default.model("restaurant", restaurantSchema);
+
 exports.default = Restaurants;
 //# sourceMappingURL=RestaurantSchema.js.map
